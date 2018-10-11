cada5c5f94f1afa71e307777f8f443cc0c997e1e
Jaroslav Šmolík <grissius@gmail.com>
2018-10-11 09:11:39 +0200

    Add basic tests

---
+++ src/test/toJson.test.ts
@@ -0,0 +1,35 @@
+import toJson from 'lib/toJson';
+
+let toJsonCounter = 0;
+let lastOptions: object;
+const jsonRes = { name: 'Jack' };
+const entity = {
+    toJSON: (options: object) => {
+        toJsonCounter++;
+        lastOptions = options;
+        return jsonRes;
+    },
+};
+
+describe('toJson', () => {
+    beforeEach(() => {
+        toJsonCounter = 0;
+    });
+
+    test('Converted to JSON', () => {
+        expect(toJson(entity)).toBe(jsonRes);
+        expect(toJsonCounter).toBe(1);
+    });
+
+    test('Converted to JSON with options', () => {
+        const options = { shallow: true };
+        expect(toJson(entity, options)).toBe(jsonRes);
+        expect(lastOptions).toBe(options);
+        expect(toJsonCounter).toBe(1);
+    });
+
+    test('Handles null', () => {
+        const nullEntity = null;
+        expect(toJson(nullEntity)).toBe(null);
+    });
+});
