4d7ee15c7b5cb9b9a53e289db97139edca23536a
Jaroslav Šmolík <grissius@gmail.com>
2018-10-11 17:00:59 +0200

    Rename param obj -> model

--- src/lib/toJson.ts
+++ src/lib/toJson.ts
@@ -6,15 +6,15 @@ interface Model {
 /**
  * Transforms (usually database) object to pure JSON object using toJSON member function.
  * If it does not have toJSON or object does not exist, it returns object itself.
- * @param obj Object to transform to JSON
+ * @param model Object to transform to JSON
  * @param options Object that is passed to toJSON method
  * @returns toJSON result
  */
-const toJson = (obj: Nullable<Model>, options: object = {}) => {
-    if (obj && obj.toJSON) {
-        return obj.toJSON(options);
+const toJson = (model: Nullable<Model>, options: object = {}) => {
+    if (model && model.toJSON) {
+        return model.toJSON(options);
     }
-    return obj;
+    return model;
 };
 
 export default toJson;
