5abf83c079b7d72bc89f086f6de073ee72d8f310
Jaroslav Šmolík <grissius@gmail.com>
2018-10-12 10:44:20 +0200

    Add params

--- src/lib/hash.ts
+++ src/lib/hash.ts
@@ -1,9 +1,9 @@
-import { createHash } from 'crypto';
+import { createHash, HexBase64Latin1Encoding } from 'crypto';
 
-const hash = (data: string) => {
-    const sha = createHash('sha512');
+const hash = (data: string, algorithm: string = 'sha512', encoding: HexBase64Latin1Encoding = 'hex') => {
+    const sha = createHash(algorithm);
     sha.update(data);
-    return sha.digest('hex');
+    return sha.digest(encoding);
 };
 
 export default hash;
