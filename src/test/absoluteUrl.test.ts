4537b4cf39da970e5ca945247532cd2940ce9ecf
Jaroslav Šmolík <grissius@gmail.com>
2018-10-12 08:55:06 +0200

    Add tests

---
+++ src/test/absoluteUrl.test.ts
@@ -0,0 +1,30 @@
+import absoluteUrl, { Request } from 'lib/absoluteUrl';
+import * as url from 'url';
+
+const createRequest = (params: {[k: string]: any}) => ({
+    get: (param: string) => params[param],
+    originalUrl: params.originalUrl,
+    protocol: params.protocol,
+});
+
+describe('absoluteUrl', () => {
+    test('Composes URL', () => {
+        const req = createRequest({
+            host: 'example.com',
+            originalUrl: '/posts/123',
+            protocol: 'http',
+        });
+        expect(absoluteUrl(req)).toBe('http://example.com/posts/123');
+    });
+    test('Composes URL the same way as url.format', () => {
+        const protocol = 'ftp';
+        const host = 'unicorn@foo.bar:8800';
+        const originalUrl = '/home';
+        const req = createRequest({ protocol, host, originalUrl });
+        expect(absoluteUrl(req)).toBe(url.format({
+            host,
+            protocol,
+            pathname: originalUrl,
+        }));
+    });
+});
