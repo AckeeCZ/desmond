bad57b51d2079adc7dff0234b43aedf061b295ca
Jaroslav Šmolík <grissius@gmail.com>
2018-10-12 12:52:37 +0200

    Rename bool to parseBool

--- src/lib/parseBool.ts
+++ src/lib/parseBool.ts
@@ -6,6 +6,6 @@ const isNan = (value: any) => isNaN(value) && typeof value !== 'string';
 /**
  * Parse bool from string (`"0"`, `"false"` --> `false`, else `true`), number (`0` --> `false`, else `true`), or boolean
  */
-const bool = (value: any) => !(isNan(value) || String(value) === 'false' || String(value) === '0');
+const parseBool = (value: any) => !(isNan(value) || String(value) === 'false' || String(value) === '0');
 
-export default bool;
+export default parseBool;
