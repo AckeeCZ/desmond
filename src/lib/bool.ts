64650246a28b1fc92101338f9a8af195c0dcb675
Jaroslav Šmolík <grissius@gmail.com>
2018-10-12 12:51:38 +0200

    Add comments

--- src/lib/bool.ts
+++ src/lib/bool.ts
@@ -1,6 +1,11 @@
-
+/**
+ * @ignore
+ */
 const isNan = (value: any) => isNaN(value) && typeof value !== 'string';
 
+/**
+ * Parse bool from string (`"0"`, `"false"` --> `false`, else `true`), number (`0` --> `false`, else `true`), or boolean
+ */
 const bool = (value: any) => !(isNan(value) || String(value) === 'false' || String(value) === '0');
 
 export default bool;
