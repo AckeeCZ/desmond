f62aefb47e603be023e9c6dafee3324946614500
Jaroslav Šmolík <grissius@gmail.com>
2018-10-15 17:55:20 +0200

    Add depricate wrapper

---
+++ src/lib/internal/deprecate.ts
@@ -0,0 +1,30 @@
+const aliases: { [k: string]: string } = {
+    absoluteUrl: 'absoluteUrl',
+    nullOrToJSON: 'toJson',
+    snakelize: 'snakelizeKeys',
+};
+
+const report = (fn: (p: any) => any) => {
+    const alias = aliases[fn.name];
+    const deprication = `Backend toolback [depricated]: ${fn.name} is depricated and will be removed.`;
+    const message = deprication + (alias ? ` Use ${alias} instead.` : '');
+    // tslint:disable-next-line
+    console.warn(message);
+};
+
+function deprecate<TResult>(fn: () => TResult): () => TResult;
+function deprecate<T1, TResult>(fn: (param1: T1) => TResult): (param1: T1) => TResult;
+function deprecate<T1, T2, TResult>(fn: (param1: T1, param2: T2) => TResult): (param1: T1, param2: T2) => TResult;
+function deprecate<T1, T2, T3, TResult>(fn: (param1: T1, param2: T2, param3: T3) => TResult): (param1: T1, param2: T2, param3: T3) => TResult;
+function deprecate<T1, T2, T3, T4, TResult>(fn: (param1: T1, param2: T2, param3: T3, param4: T4) => TResult):
+    (param1: T1, param2: T2, param3: T3, param4: T4) => TResult;
+function deprecate<TParam, TResult>(fn: (...params: TParam[]) => TResult): (...params: TParam[]) => TResult {
+    return (...params) => {
+        report(fn);
+        return fn(...params);
+    };
+}
+
+export {
+    deprecate
+};
