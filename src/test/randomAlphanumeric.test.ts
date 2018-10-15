2360fd0dacf2ebc32eaee9125964eb57b2b0790a
Jaroslav Šmolík <grissius@gmail.com>
2018-10-15 11:34:25 +0200

    Fix coding style

--- src/test/randomAlphanumeric.test.ts
+++ src/test/randomAlphanumeric.test.ts
@@ -5,13 +5,14 @@ describe('randomAlfanumeric', () => {
         expect(randomAfanumeric(0)).toBe('');
     });
     test('Length matches', () => {
-        Array(32).fill(null).forEach((_,i) => {
+        Array(32).fill(null).forEach((_, i) => {
             expect(randomAfanumeric(i)).toHaveLength(i);
-        })
+        });
     });
     test('Large alfanum contains only alfanum', () => {
-        const longRand = randomAfanumeric(512);
-        expect(longRand).toMatch(/^[a-z0-9]+$/i)
-        expect(longRand).toHaveLength(512);
+        const n = 512;
+        const longRand = randomAfanumeric(n);
+        expect(longRand).toMatch(/^[a-z0-9]+$/i);
+        expect(longRand).toHaveLength(n);
     });
 });
