3e390b19af834829e156cc488cf65c85188323f9
Jaroslav Šmolík <grissius@gmail.com>
2018-10-15 18:00:09 +0200

    Rename generateRandomAlphanumeric

--- src/test/generateRandomAlphanumeric.test.ts
+++ src/test/generateRandomAlphanumeric.test.ts
@@ -1,17 +1,17 @@
-import randomAfanumeric from 'lib/randomAfanumeric';
+import generateRandomAlphanumeric from 'lib/generateRandomAlphanumeric';
 
 describe('randomAlfanumeric', () => {
     test('Zero length', () => {
-        expect(randomAfanumeric(0)).toBe('');
+        expect(generateRandomAlphanumeric(0)).toBe('');
     });
     test('Length matches', () => {
         Array(32).fill(null).forEach((_, i) => {
-            expect(randomAfanumeric(i)).toHaveLength(i);
+            expect(generateRandomAlphanumeric(i)).toHaveLength(i);
         });
     });
     test('Large alfanum contains only alfanum', () => {
         const n = 512;
-        const longRand = randomAfanumeric(n);
+        const longRand = generateRandomAlphanumeric(n);
         expect(longRand).toMatch(/^[a-z0-9]+$/i);
         expect(longRand).toHaveLength(n);
     });
