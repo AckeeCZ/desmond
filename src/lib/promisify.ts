7bcd54ecea7f1b96afae225477806681702f9655
Jaroslav Šmolík <grissius@gmail.com>
2018-10-16 17:11:35 +0200

    Add promisify

---
+++ src/lib/promisify.ts
@@ -0,0 +1,22 @@
+export type Callback<T> = (e: any, res?: T) => void;
+
+function promisify<TRes>(fn: (cb: Callback<TRes>) => any): () => Promise<TRes>;
+function promisify<T1, TRes>(fn: (p1: T1, cb: Callback<TRes>) => any): (a1: T1) => Promise<TRes>;
+function promisify<T1, T2, TRes>(fn: (p1: T1, p2: T2, cb: Callback<TRes>) => any): (a1: T1, a2: T2) => Promise<TRes>;
+function promisify<T1, T2, T3, TRes>(fn: (p1: T1, p2: T2, p3: T3, cb: Callback<TRes>) => any): (a1: T1, a2: T2, a3: T3) => Promise<TRes>;
+function promisify<T1, T2, T3, T4, TRes>(fn: (p1: T1, p2: T2, p3: T3, p4: T4, cb: Callback<TRes>) => any): (a1: T1, a2: T2, a3: T3, a4: T4) => Promise<TRes>;
+
+function promisify(callbackFunction: any) {
+  return (...args: any[]) =>
+    new Promise((resolve, reject) => {
+      const cb: Callback<any> = (err, result) => {
+        if (err) {
+          return reject(err);
+        }
+        return resolve(result);
+      };
+      callbackFunction.apply(null, [...args, cb]);
+    });
+}
+
+export default promisify;
---
+++ src/test/promisify.test.ts
@@ -0,0 +1,19 @@
+import promisify, { Callback } from 'lib/promisify';
+
+const cbSimple = (ret: number, cb: Callback<number>) => cb(null, ret);
+const cbElab = (a: any, err: string, ret: string, fail: boolean, cb: Callback<string>) => fail ? cb(err) : cb(null, ret);
+
+describe('promisify', () => {
+    test('One param resolve', async () => {
+        const n = 42;
+        await expect(promisify(cbSimple)(n)).resolves.toBe(n);
+    });
+    test('Multiple param resolve', async () => {
+        const ret = 'bazBarBax';
+        await expect(promisify(cbElab)(null, 'err', ret, false)).resolves.toBe(ret);
+    });
+    test('Multiple param resolve', async () => {
+        const err = 'bazBarBax';
+        await expect(promisify(cbElab)(null, err, 'ret', true)).rejects.toBe(err);
+    });
+});
