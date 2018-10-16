5ab048286becb9ccebfabbb76ba3e97a1c54783b
Jaroslav Šmolík <grissius@gmail.com>
2018-10-16 17:42:30 +0200

    Add tap

---
+++ src/lib/tap.ts
@@ -0,0 +1,9 @@
+/**
+ * Promise side effect.
+ * Inspired by common bluebird's Promise#tap
+ */
+const tap = <T>(handler: (param: T) => any) =>
+  (value: T) => Promise.resolve(handler(value))
+    .then(() => value);
+
+export default tap;
