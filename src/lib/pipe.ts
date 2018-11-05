12576be6ee5e1e0aeb8b0e31807006130ab50d64
Jaroslav Šmolík <grissius@gmail.com>
2018-11-05 16:34:01 +0100

    Use `function` instead of arrow for pipe

--- src/lib/pipe.ts
+++ src/lib/pipe.ts
@@ -1,11 +1,13 @@
-const pipe = (...fns: any[]) => (...initialArgs: any) =>
-    fns.reduce((lastResult, fn, i) => lastResult.then((res: any) => {
-        const currentResult = fn(
-            // The first function receives all the arguments passed to the composed function
-            // The others just the one result from the previous one.
-            ...(i ? [res] : res)
-        );
-        return Array.isArray(currentResult) ? Promise.all(currentResult) : currentResult;
-    }), Promise.all(initialArgs));
+function pipe(...fns: any[]) {
+    return (...initialArgs: any) =>
+        fns.reduce((lastResult, fn, i) => lastResult.then((res: any) => {
+            const currentResult = fn(
+                // The first function receives all the arguments passed to the composed function
+                // The others just the one result from the previous one.
+                ...(i ? [res] : res)
+            );
+            return Array.isArray(currentResult) ? Promise.all(currentResult) : currentResult;
+        }), Promise.all(initialArgs));
+}
 
 export default pipe;
