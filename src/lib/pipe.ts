5f55c194f1bc3c91ae45060c79baa6e7f560e850
Jaroslav Šmolík <grissius@gmail.com>
2018-11-05 16:23:04 +0100

    Simplify returns

--- src/lib/pipe.ts
+++ src/lib/pipe.ts
@@ -1,15 +1,12 @@
-const pipe = (() => {
-    return (...fns: any[]) => (...args: any) =>
-        fns.reduce((lastResult, fn, i) => {
-            return lastResult.then((res: any) => {
-                const nextResult = fn(
-                    // The first function receives all the arguments passed to the composed function
-                    // The others just the one result from the previous one.
-                    ...(i ? [res] : res)
-                );
-                return Array.isArray(nextResult) ? Promise.all(nextResult) : nextResult;
-            });
-        }, Promise.all(args));
-})();
+const pipe = (() => (...fns: any[]) => (...args: any) =>
+    fns.reduce((lastResult, fn, i) => lastResult.then((res: any) => {
+        const nextResult = fn(
+            // The first function receives all the arguments passed to the composed function
+            // The others just the one result from the previous one.
+            ...(i ? [res] : res)
+        );
+        return Array.isArray(nextResult) ? Promise.all(nextResult) : nextResult;
+    }), Promise.all(args))
+)();
 
 export default pipe;
