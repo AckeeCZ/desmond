8c5f30a035cf2a225907fb58f26b230401d78de6
Jaroslav Šmolík <grissius@gmail.com>
2018-11-05 16:32:08 +0100

    Rename vars

--- src/lib/pipe.ts
+++ src/lib/pipe.ts
@@ -1,11 +1,11 @@
-const pipe = (...fns: any[]) => (...args: any) =>
+const pipe = (...fns: any[]) => (...initialArgs: any) =>
     fns.reduce((lastResult, fn, i) => lastResult.then((res: any) => {
-        const nextResult = fn(
+        const currentResult = fn(
             // The first function receives all the arguments passed to the composed function
             // The others just the one result from the previous one.
             ...(i ? [res] : res)
         );
-        return Array.isArray(nextResult) ? Promise.all(nextResult) : nextResult;
-    }), Promise.all(args));
+        return Array.isArray(currentResult) ? Promise.all(currentResult) : currentResult;
+    }), Promise.all(initialArgs));
 
 export default pipe;
