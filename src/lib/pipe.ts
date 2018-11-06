83753d294245f1520836cf6eacafbfa97815bbb1
Jaroslav Šmolík <grissius@gmail.com>
2018-11-06 11:18:19 +0100

    Add pipe docs

--- src/lib/pipe.ts
+++ src/lib/pipe.ts
@@ -47,6 +47,39 @@ function pipe<D1, D2, D3, D4, D5, DRes, R1, R2, R3, R4, R5>(
     r5: RiverFn<R4, R5>
 ): ReplaceReturnTypePromise<DeltaFn<D1, D2, D3, D4, D5, DRes>, R5>;
 
+/**
+ * Create a function composed of provided functions in left-to-right execution chain.
+ * Resulting function arguments are identical to the arguments of the first function
+ * and return value identical to the result value of the last function in the chain.
+ * ```typescript
+ * const myPipe = pipe(
+ *     (x: string) => x + 'first ',
+ *     (x: string) => x + 'second',
+ * );
+ * await myPipe('Called: ') // 'Called: first second'
+ * ```
+ * First function can have multiple arguments:
+ * ```typescript
+ * await pipe(
+ *     (a: number, b: number) => a + b,
+ *     String,
+ * )(1, 2); // '3'
+ * ```
+ * In-between the steps, all result promises are resolved. If result is an array, elements are resolved via Promise.all
+ * ```typescript
+ * await pipe(
+ *     // first function has 0 args, resulting pipe has 0 args
+ *     // any function can return promise, next function receives value as arg
+ *     () => Promise.resolve([1, 2, 5, 8])
+ *     // returning array of promises
+ *     // no need to wrap in promise resolved
+ *     (ids: number[]) => ids.map(db.find),
+ *     // resolved array of users ready in next function
+ *     (users: User[]), => users.map(countUserRating),
+ * )(); // userRating[]
+ * ```
+ * @return Function with signature of the first function (or no args if no functions provided) returning Promise of a result of last function
+ */
 function pipe(...fns: any[]): any {
     return (...initialArgs: any) =>
         fns.reduce((pendingLastResult, fn, i) => pendingLastResult.then((lastResult: any) => {
