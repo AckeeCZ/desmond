5f77aa308b2a4534afa856cf9dcaaa8e14e54b14
Jaroslav Šmolík <grissius@gmail.com>
2018-11-07 17:44:18 +0100

    Update docs

--- src/lib/snakelize.ts
+++ src/lib/snakelize.ts
@@ -1,17 +1,17 @@
 import snakeCase from 'lodash.snakecase';
 import { mapKeys } from './internal/utils';
 import { isString } from './internal/validators';
-/**
- * Snakelize keys of given object(s). Only top level keys are transformed.
- * Keys are assumed to be camelCase.
- * @param input object(s) to transform
- */
 function snakelize(string: string): string;
 function snakelize(object: { [key: string]: any }): { [key: string]: any };
 function snakelize(array: Array<{ [key: string]: any }>): Array<{ [key: string]: any }>;
+/**
+ * Snakelize string or keys of given object(s) if provided. Only top level keys are transformed when handling object(s).
+ * Keys are assumed to be camelCase.
+ * @param input object(s) to transform
+ */
 function snakelize(input: any) {
     if (isString(input)) {
-        return snakeCase(input);
+        return snakeCase();
     }
     return Array.isArray(input) ? input.map(o => mapKeys(o, snakeCase)) : mapKeys(input, snakeCase);
 };
