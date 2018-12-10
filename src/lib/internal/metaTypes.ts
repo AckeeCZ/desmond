bf576db5a0f852eb84aa40caf7248db7a36346a7
Jaroslav Šmolík <grissius@gmail.com>
2018-12-10 08:12:58 +0100

    Fix Tail for tuples

--- src/lib/internal/metaTypes.ts
+++ src/lib/internal/metaTypes.ts
@@ -3,7 +3,7 @@ export type ArgType<F, Else= never> = F extends (arg: infer A) => any ? A : Else
 export type ReplaceReturnTypePromise<T, TNewReturn> = (...a: ArgTypes<T>) => Promise<TNewReturn>;
 export type VariadicFunction = (...args: any[]) => any;
 export type Lookup<T, K, Default= never> = K extends keyof T ? T[K] : Default;
-export type Tail<T extends any[]> = T extends [any, ...Array<infer U>] ? U[] : never;
+export type Tail<T extends any[]> = ((...t: T) => void) extends ((x: any, ...u: infer U) => void) ? U : never;
 export type Head<T extends any[]> = T extends [infer U, ...any[]] ? U : never;
 export type UnpackPromise<T> = T extends Promise<Array<infer U>> ? U[] : T extends Promise<infer D> ? D : T;
 export type LastIndexOf<T extends any[]> =
