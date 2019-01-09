9c23c3378d504813bb6c59884444c4e4ab18441c
Jaroslav Šmolík <grissius@gmail.com>
2019-01-09 15:40:00 +0100

    Add attributes test

---
+++ src/test/attributes.test.ts
@@ -0,0 +1,22 @@
+import attributes from 'lib/attributes';
+
+const data = {
+    someIds: '[1,2,3,4]',
+    active: 1,
+    boring: 0,
+    nullThis: '',
+    keepThis: [],
+};
+
+describe('attributes', () => {
+    test('Is unchanged without options', () => {
+        expect(attributes(data)).toEqual(data);
+    });
+    test('Converts JSON, bool and omits nulls', () => {
+        expect(attributes(data, {
+            jsonColumns: ['someIds'],
+            toBoolean: ['active', 'boring'],
+            nullOnEmpty: ['nullThis'],
+        })).toMatchSnapshot();
+    });
+});
