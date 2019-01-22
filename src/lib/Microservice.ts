b98cd94fb24cd8f971c0fbe2447c2a3c731b08ac
Jaroslav Šmolík <grissius@gmail.com>
2019-01-22 09:42:24 +0100

    Refactor request using async await

--- src/lib/Microservice.ts
+++ src/lib/Microservice.ts
@@ -87,7 +87,7 @@ export default class Microservice {
         return url.resolve(this.baseUrl, pathName);
     }
 
-    protected request(pathName: string, options: any = {}) {
+    protected async request(pathName: string, options: any = {}) {
         const uri = this.composeUrl(pathName);
         const correlationId = Math.random()
             .toString(36)
@@ -96,34 +96,31 @@ export default class Microservice {
         // although the resolved `uri` could be without it - if `pathName` is equal to '/'
         this.logger.info({ uri, pathName, correlationId, qs: options.qs, body: options.body }, `--> ${this.baseUrl}`);
         const tStart = stopwatch.start();
-        return Promise.resolve(
-            request(
+        try {
+            const res = await request(
                 defaultsDeep({}, this.defaultOptions, options, {
                     uri,
                     resolveWithFullResponse: true,
                 })
-            )
-        )
-            .then((res: Response) => {
-                const millis = stopwatch.stop(tStart);
-                // TODO What to log
-                //  Optional body logger per call?
-                //  Dont log large bodies at all? (and size only instead of contents)
-                //  Headers?
-                this.logger.info(
-                    { correlationId, statusCode: res.statusCode, body: res.body },
-                    `<-- ${this.baseUrl} ${res.statusCode} (${Math.round(millis)}ms)`
-                );
-                return res;
-            })
-            .catch(error => {
-                const millis = stopwatch.stop(tStart);
-                this.logger.error(
-                    { correlationId, error, message: error.message },
-                    `-->X ${this.baseUrl} (${Math.round(millis)}ms)`
-                );
-                throw error;
-            });
+            );
+            const millis = stopwatch.stop(tStart);
+            // TODO What to log
+            //  Optional body logger per call?
+            //  Dont log large bodies at all? (and size only instead of contents)
+            //  Headers?
+            this.logger.info(
+                { correlationId, statusCode: res.statusCode, body: res.body },
+                `<-- ${this.baseUrl} ${res.statusCode} (${Math.round(millis)}ms)`
+            );
+            return res;
+        } catch (error) {
+            const millis = stopwatch.stop(tStart);
+            this.logger.error(
+                { correlationId, error, message: error.message },
+                `-->X ${this.baseUrl} (${Math.round(millis)}ms)`
+            );
+            throw error;
+        }
     }
 
     public get(pathName: string = '/', qs: object = {}, options: object = {}) {
